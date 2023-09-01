f4090f0166369e1d6af7250f71e4faf8e4bb3de9
Jefferson Gomes <117922731+Jeffergs@users.noreply.github.com>
2023-09-01 11:50:17 -0300

    Update Conversor_de_Dolar_para_Real.py

--- Conversor_de_Dolar_para_Real.py
+++ Conversor_de_Dolar_para_Real.py
@@ -1,8 +1,9 @@
 # Tendo como base o valor da cotação do dólar (em reais) do dia, faça a conversão de um valor em dólares para reais
 
-valor_cotacao = float(input("Digite o valor da cotação do dólar: "
-dolar = float(input("Quantos dolares você tem: "))
+valor_cotacao = float(input("Digite o valor da cotação do dólar: "))
+dolar = float(input("Quantos dolares você tem: "));
 
 total = dolar * valor_cotacao
 
-print(f"USD{valor_dolares:.2f} convertido para reais é R$ {total}")
+print(f"Você tem R$ {total:.2f} reais");
+print(f"{dolar:.2f} dolares convertido para reais é R$ {total}");
