efb4aa87dd2946eab1beef9ce7b31d36204ea11d
Jefferson Gomes <117922731+Jeffergs@users.noreply.github.com>
2023-08-31 22:46:00 -0300

    Update Perimetro_do_triangulo.py

--- Perimetro_do_triangulo.py
+++ Perimetro_do_triangulo.py
@@ -1,9 +1,9 @@
 # Dados 3 lados de um triângulo, calcule e mostre seu perímetro (soma dos lados).
 
-l1 = float(input("Quantos centímetros mede este lado? "))
-l2 = float(input("Quantos centímetros mede este lado? "))
-l3 = float(input("Quantos centímetros mede este lado? "))
+l1 = float(input("Quantos centímetros o primeiro lado? "))
+l2 = float(input("Quantos centímetros o segundo  lado? "))
+l3 = float(input("Quantos centímetros o terceiro  lado? "))
 
 perimetro = l1 + l2 + l3
 
-print(f"O perímetro do triângulo mede {perimetro:.2f} centímetros ")
+print(f"O perímetro do triângulo mede {perimetro:.2f} ")
