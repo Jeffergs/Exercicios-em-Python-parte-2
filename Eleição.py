8796cfdf5dc2a5b4c19b723f82d1874cff0e0f90
Jefferson Gomes <117922731+Jeffergs@users.noreply.github.com>
2023-08-31 22:58:03 -0300

    Update Eleição.py

--- Eleição.py
+++ Eleição.py
@@ -1,12 +1,12 @@
 # 1°-Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos.
 # 2°-Calcule e escrever o percentual que cada um representa em relação ao total de eleitores.
 
-
-total_eleitores = int(input("Total de eleitores: "))
-
 votos_brancos = int(input("Total de votos brancos: "))
 votos_nulos = int(input("Total de votos nulos: "))
 votos_validos = int(input("Total de votos válidos: "))
+
+total_eleitores = votos_brancos + votos_nulos + votos_validos
+print(f"O total de votos é {total_eleitores:.2f} ")
 
 brancos = (votos_brancos * 100) / total_eleitores
 nulos = (votos_nulos * 100) / total_eleitores
