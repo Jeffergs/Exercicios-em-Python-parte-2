a773c3a8c724708accc468e9f87e986b18e5e348
Jefferson Gomes <117922731+Jeffergs@users.noreply.github.com>
2023-08-31 22:50:44 -0300

    Update Idade_dias.py

--- Idade_dias.py
+++ Idade_dias.py
@@ -1,13 +1,12 @@
-# Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias.
+# Faça um algoritmo que leia a idade de uma pessoa expressa em anos, 
+# meses e dias e escreva a idade dessa pessoa expressa apenas em dias.
 # Considerar ano com 365 dias e mês com 30 dias.
 
-anos = int(input("Idade (anos): ")) # Quantos anos de vida a pessoa tem
-meses = int(input("Idade (meses): ")) # Quantos meses (a partir do último aniversário) a pessoa tem
-dias = int(input("Idade (dias): ")) # Quantos dias (a partir do último aniversário) a pessoa tem
+anos = int(input("Idade (em anos): ")) # Quantos anos de vida a pessoa tem
+meses = int(input("Idade (em meses): ")) # Quantos meses (a partir do último aniversário) a pessoa tem
+dias = int(input("Idade (em dias): ")) # Quantos dias (a partir do último aniversário) a pessoa tem
 
-a = anos * 365 # anos * 365 dias
-b = meses * 30 # meses * 30 dias
-dias_vividos = a + b + dias
+dias_vividos = (anos * 365) + (meses * 30)# + dias
 
 print(f"Você está vivo a {dias_vividos} dias ")
 
